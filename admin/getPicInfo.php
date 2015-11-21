b09c102bbee92191c9a5d4dea0d968ff417165f7
Constantin Kraft <ckraft@smail.uni-koeln>
2015-11-21 15:42:19 +0100

    - Replaced mysql* with mysqli* functions

--- admin/getPicInfo.php
+++ admin/getPicInfo.php
@@ -15,9 +15,9 @@ if(!isset($_GET['id']) OR !is_numeric($_GET['id']))
 
 $sql = "SELECT * FROM pictures WHERE id=$_GET[id]";
 
-if($query = mysql_query($sql))
+if($query = mysqli_query($connection, $sql))
 {
-    $data = mysql_fetch_array($query);
+    $data = mysqli_fetch_array($query);
     echo "<div id=\"picInfo\">";
     echo "<img src=\"$data[datei_pic]\" alt=\"$data[titel]\" />";
     echo "<h2>$data[titel]</h2>";
